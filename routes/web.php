b144852b9919bd395a0adc51a6dca3d84ed531f7
Evgeniy <evgeniy.web@ukr.net>
2017-06-10 17:58:03 +0300

    11-A-User-Can-Filter-Threads-By-Channel

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('threads', 'ThreadsController@index')->name('all_threads');
+Route::get('threads/create', 'ThreadsController@create');
+Route::get('threads/{channel}', 'ThreadsController@index')->name('threads_channel');
+Route::post('threads', 'ThreadsController@store');
+Route::get('threads/{thread}', 'ThreadsController@show');
+Route::get('threads/{channel}/{thread}', 'ThreadsController@show')->name('home');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
-//Route::resource('threads', 'ThreadsController');
-Route::get('/threads', 'ThreadsController@index')->name('all_threads');
-Route::post('/threads', 'ThreadsController@store');
-Route::get('/threads/create', 'ThreadsController@create');
-Route::get('/threads/{thread}', 'ThreadsController@show');
-Route::get('/threads/{channel}/{thread}', 'ThreadsController@show')->name('home');
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('home', 'HomeController@index')->name('home');
 
 
--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Channel;
 use App\Reply;
 use App\Thread;
 use Tests\TestCase;
@@ -42,5 +43,22 @@ class ReadThreadsTest extends TestCase
             ->create(['thread_id' => $this->thread->id]);
         $this->get($this->thread->path())
             ->assertSee($repl->body);
+    }
+
+    /** @test */
+    public function a_user_can_filter_threads_according_to_a_channel()
+    {
+        $channel = create(Channel::class);
+        $threadInChannel = create(
+            Thread::class,
+            ['channel_id' => $channel->id]
+        );
+        $threadNotInChannel = create(
+            Thread::class
+        );
+
+        $this->get("/threads/{$channel->slug}")
+            ->assertSee($threadInChannel->title)
+            ->assertDontSee($threadNotInChannel->title);
     }
 }
